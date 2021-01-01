b556972f6a6c9ed8c5b9ef80cb17f50dc208dca1
yuhyun <dbgus101@gmail.com>
2021-01-01 18:33:42 +0900

    update header and slider

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,13 +6,13 @@ function Header() {
     //    <video muted autoPlay loop src="https://dronehi.s3.ap-northeast-2.amazonaws.com/background-video/201212-%EC%84%9C%EC%9A%B8+%EC%9D%BC%EB%AA%B03.mp4">
 <div className="Header">
           <div className="logo">
-          <a>DRONE HI</a>
+          <li>DRONE HI</li>
           </div>
           <div className="menu">
-          <a>ABOUT</a>
-          <a>SERVICE</a>
-          <a>PORTFOLIO</a>
-          <a>CONTACT</a>
+          <li>ABOUT</li>
+          <li>SERVICE</li>
+          <li>PORTFOLIO</li>
+          <li>CONTACT</li>
           </div>
       </div>
 
